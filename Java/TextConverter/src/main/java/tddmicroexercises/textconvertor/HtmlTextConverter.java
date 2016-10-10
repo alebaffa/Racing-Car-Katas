c2e75639bb0c8cb11be5c8a3636626eeae2e9a27
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-10-10 21:51:38 +0200

    parameterize method solution

--- Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -1,35 +1,32 @@
 package tddmicroexercises.textconvertor;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
-public class HtmlTextConverter
-{
+public class HtmlTextConverter {
     private String fullFilenameWithPath;
 
-    public HtmlTextConverter(String fullFilenameWithPath)
-    {
+    public HtmlTextConverter(String fullFilenameWithPath) {
         this.fullFilenameWithPath = fullFilenameWithPath;
     }
 
-    public String convertToHtml() throws IOException{
-    
-	    BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
-	    
-	    String line = reader.readLine();
-	    String html = "";
-	    while (line != null)
-	    {
-	    	html += StringEscapeUtils.escapeHtml(line);
-	        html += "<br />";
-	        line = reader.readLine();
-	    }
-	    return html;
-
+    public String convertToHtml() throws IOException {
+        return convertToHtml(new FileReader(fullFilenameWithPath));
     }
 
-	public String getFilename() {
-		return this.fullFilenameWithPath;
-	}
+    protected String convertToHtml(Reader reader) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        String line = bufferedReader.readLine();
+        String html = "";
+        while (line != null) {
+            html += StringEscapeUtils.escapeHtml(line);
+            html += "<br />";
+            line = bufferedReader.readLine();
+        }
+        return html;
+    }
+
+    public String getFilename() {
+        return this.fullFilenameWithPath;
+    }
 }
--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -2,6 +2,10 @@ package tddmicroexercises.textconvertor;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -10,6 +14,15 @@ public class HtmlTextConverterTest {
     public void should_return_the_filename() {
         HtmlTextConverter converter = new HtmlTextConverter("foo");
         assertThat(converter.getFilename(), is("foo"));
+    }
+
+    @Test
+    public void should_return_the_string_converted() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter("");
+
+        Reader stringReader = new StringReader("foo");
+
+        assertThat(converter.convertToHtml(stringReader), is("foo<br />"));
     }
 
 }
